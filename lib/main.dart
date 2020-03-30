601ffe493b1b6bf2bd41830f561c4ef557a78058
Lamiss Messaoudi <younglamiss@gmail.com>
2020-03-30 18:34:09 +0200

    Adding Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './screens/filter_screen.dart';
 import './screens/tabs_screen.dart';
 import './screens/categories_screen.dart';
 import './screens/category_meals_screen.dart';
@@ -33,6 +34,7 @@ class MyApp extends StatelessWidget {
       home: TabsScreen(),
       routes: {
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        FilterScreen.routeName: (ctx) => FilterScreen(),
         MealDeatilScreen.routeName: (ctx) => MealDeatilScreen(),
       },
 
---
+++ lib/screens/filter_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FilterScreen extends StatelessWidget {
+  static const routeName = '/filters';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('filters !!'),
+      ),
+      appBar: AppBar(
+        title: Text('Filters'),
+      ),
+    );
+  }
+}
